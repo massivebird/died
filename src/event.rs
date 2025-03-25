7c5e0be7e90b91e28fbb2d1f83dfb815804c0581
massivebird <33991373+massivebird@users.noreply.github.com>
2025-03-25 12:55:00 -0400

    refactor: new event module

---
+++ src/event.rs
@@ -0,0 +1,43 @@
+use regex::Regex;
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub struct Event {
+    pub name: String,
+    pub weight: u32,
+}
+
+impl TryFrom<&str> for Event {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let re = Regex::new(r"(?<name>\S+):(?<weight>\d+)$").unwrap();
+
+        let Some(caps) = re.captures(value) else {
+            return Ok(Self {
+                name: value.to_string(),
+                weight: 1,
+            });
+        };
+
+        let name = caps.name("name").unwrap().as_str();
+
+        let weight = caps
+            .name("weight")
+            .unwrap()
+            .as_str()
+            .parse::<u32>()
+            .unwrap();
+
+        Ok(Self {
+            name: name.to_string(),
+            weight,
+        })
+    }
+}
+
+impl Display for Event {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,8 @@
 use clap::{Arg, ArgAction};
-use regex::Regex;
-use std::{fmt::Display, process};
+use event::Event;
+use std::process;
 
-#[derive(Debug)]
-struct Event {
-    name: String,
-    weight: u32,
-}
-
-impl TryFrom<&str> for Event {
-    type Error = String;
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let re = Regex::new(r"(?<name>\S+):(?<weight>\d+)$").unwrap();
-
-        let Some(caps) = re.captures(value) else {
-            return Ok(Self {
-                name: value.to_string(),
-                weight: 1,
-            });
-        };
-
-        let name = caps.name("name").unwrap().as_str();
-
-        let weight = caps
-            .name("weight")
-            .unwrap()
-            .as_str()
-            .parse::<u32>()
-            .unwrap();
-
-        Ok(Self {
-            name: name.to_string(),
-            weight,
-        })
-    }
-}
-
-impl Display for Event {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.name)
-    }
-}
+mod event;
 
 fn main() {
     let matches = clap::command!()
