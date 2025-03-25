d04621d539a4c965456ee149f2001b651af88937
massivebird <33991373+massivebird@users.noreply.github.com>
2025-03-25 12:35:33 -0400

    added: Event struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 use clap::{Arg, ArgAction};
 use regex::Regex;
-use std::fmt::Display;
-use std::process;
+use std::{fmt::Display, process};
 
 #[derive(Debug)]
 struct Event {
     name: String,
     weight: u32,
+}
+
+impl TryFrom<&str> for Event {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let re = Regex::new(r"(?<name>\S+):(?<weight>\d+)$").unwrap();
+
+        let Some(caps) = re.captures(value) else {
+            return Ok(Self {
+                name: value.to_string(),
+                weight: 1,
+            });
+        };
+
+        let name = caps.name("name").unwrap().as_str();
+
+        let weight = caps
+            .name("weight")
+            .unwrap()
+            .as_str()
+            .parse::<u32>()
+            .unwrap();
+
+        Ok(Self {
+            name: name.to_string(),
+            weight,
+        })
+    }
 }
 
 impl Display for Event {
@@ -31,42 +59,16 @@ fn main() {
 
     let d: u32 = matches.get_one("d").copied().unwrap();
 
-    let raw_events: Vec<String> = matches.get_many("events").unwrap().cloned().collect();
-
-    let mut events: Vec<Event> = Vec::new();
-
-    {
-        let re = Regex::new(r"(?<name>\S+):(?<weight>\d+)$").unwrap();
-        for raw_e in raw_events {
-            let Some(caps) = re.captures(&raw_e) else {
-                events.push(Event {
-                    name: raw_e,
-                    weight: 1,
-                });
-
-                continue;
-            };
-
-            let name = caps.name("name").unwrap().as_str();
-
-            let weight = caps
-                .name("weight")
-                .unwrap()
-                .as_str()
-                .parse::<u32>()
-                .unwrap();
-
-            events.push(Event {
-                name: name.to_string(),
-                weight,
-            });
-        }
-    }
+    let events: Vec<Event> = matches
+        .get_many("events")
+        .unwrap()
+        .cloned()
+        .collect::<Vec<String>>()
+        .iter()
+        .map(|s| Event::try_from(s.as_str()).unwrap())
+        .collect::<Vec<Event>>();
 
     let total_weight = events.iter().fold(0, |acc, e| acc + e.weight);
-
-    dbg!(&events);
-    dbg!(total_weight);
 
     let increment = d / total_weight;
 
@@ -75,11 +77,17 @@ fn main() {
         process::exit(1);
     }
 
+    println!("Dice      d{d}");
+    println!("Events    {}", events.len());
+    println!("--------------");
+
     let mut i = 0;
     for event in events {
+        // This event's lower bound.
         i += 1;
         print!("{i:2} - ");
 
+        // Calculate this event's upper bound.
         i -= 1;
         for _ in 0..event.weight {
             i += increment;
@@ -88,12 +96,10 @@ fn main() {
         println!("{i:<2}  {event}");
     }
 
-    if i == d {
-        return;
+    if i != d {
+        // Designate remaining values as rerolls.
+        i += 1;
+        print!("{i:2} - ");
+        println!("{d:<2}  [Reroll]");
     }
-
-    i += 1;
-    print!("{i:2} - ");
-
-    println!("{d:<2}  [Reroll]");
 }
