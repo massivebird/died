7c68098d41e6878ecde377c904cbcaef7b7c1986
massivebird <33991373+massivebird@users.noreply.github.com>
2025-03-25 12:59:15 -0400

    added: nix flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Dice event distribution utility";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # nixpkgs.url = "nixpkgs";
+    naersk = {
+      url = "github:nix-community/naersk/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        # for `nix build` and `nix run`:
+        packages.default = naersk-lib.buildPackage ./.;
+
+        # for `nix develop`:
+        shells.default = with pkgs;
+          mkShell {
+            buildInputs =
+              [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+      });
+}
