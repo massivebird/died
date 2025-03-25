0f32caec02717f795b48c865ec608841b1405730
massivebird <33991373+massivebird@users.noreply.github.com>
2025-03-25 12:19:34 -0400

    feat: weighted events (functional I think)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use clap::{Arg, ArgAction};
+use regex::Regex;
+use std::fmt::Display;
 use std::process;
+
+#[derive(Debug)]
+struct Event {
+    name: String,
+    weight: u32,
+}
+
+impl Display for Event {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
 
 fn main() {
     let matches = clap::command!()
@@ -17,10 +31,44 @@ fn main() {
 
     let d: u32 = matches.get_one("d").copied().unwrap();
 
-    let events: Vec<String> = matches.get_many("events").unwrap().cloned().collect();
-    let num_events = u32::try_from(events.len()).unwrap();
+    let raw_events: Vec<String> = matches.get_many("events").unwrap().cloned().collect();
 
-    let increment = d / num_events;
+    let mut events: Vec<Event> = Vec::new();
+
+    {
+        let re = Regex::new(r"(?<name>\S+):(?<weight>\d+)$").unwrap();
+        for raw_e in raw_events {
+            let Some(caps) = re.captures(&raw_e) else {
+                events.push(Event {
+                    name: raw_e,
+                    weight: 1,
+                });
+
+                continue;
+            };
+
+            let name = caps.name("name").unwrap().as_str();
+
+            let weight = caps
+                .name("weight")
+                .unwrap()
+                .as_str()
+                .parse::<u32>()
+                .unwrap();
+
+            events.push(Event {
+                name: name.to_string(),
+                weight,
+            });
+        }
+    }
+
+    let total_weight = events.iter().fold(0, |acc, e| acc + e.weight);
+
+    dbg!(&events);
+    dbg!(total_weight);
+
+    let increment = d / total_weight;
 
     if increment == 0 {
         eprintln!("ERROR: dice sides must be greater than number of events.");
@@ -32,7 +80,11 @@ fn main() {
         i += 1;
         print!("{i:2} - ");
 
-        i += increment - 1;
+        i -= 1;
+        for _ in 0..event.weight {
+            i += increment;
+        }
+
         println!("{i:<2}  {event}");
     }
 
